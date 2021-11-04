55be08827672f3011eb8f1d82203bb37463942b7
Jiri-Lenner <ji.lenner@gmail.com>
2021-11-04 19:51:09 +0100

    added animations on scroll (AOS) for all three parts

--- 00_Website/build/scripts/script.js
+++ 00_Website/build/scripts/script.js
@@ -1,15 +1,17 @@
 //navbar => sticky
 let last_scroll;
-let navbar;
-window.addEventListener("scroll", function () {
-  navbar = document.querySelector("body > header > nav");
-  last_scroll = window.scrollY;
 
-  if (window.scrollY > 20) {
-    navbar.classList.add("nav__stick");
-    //console.log(window.scrollY)
-    //=> jak daleko je od vršku
-  } else {
-    navbar.classList.remove("nav__stick");
-  }
+let navbar;
+
+window.addEventListener("scroll", function () {
+    navbar = document.querySelector("body > header > nav");
+    last_scroll = window.scrollY;
+
+    if (window.scrollY > 20) {
+        navbar.classList.add("nav__stick");
+        //console.log(window.scrollY)
+        //=> jak daleko je od vršku
+    } else {
+        navbar.classList.remove("nav__stick");
+    }
 });

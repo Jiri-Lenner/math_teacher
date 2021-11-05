5f474ec7b3bcd65f4fb3428ed7317233cd5cd1ed
Jiri-Lenner <ji.lenner@gmail.com>
2021-11-05 20:46:35 +0100

    added nav control features, nav hide feature, fixed padding and margin so it now support better scrolling, added logo, added grid part + fixed grid part

--- 00_Website/build/scripts/script.js
+++ 00_Website/build/scripts/script.js
@@ -3,6 +3,10 @@ let last_scroll;
 
 let navbar;
 
+let prevScrollpos = window.scrollY;
+
+//nav sticking to the top
+
 window.addEventListener("scroll", function () {
     navbar = document.querySelector("body > header > nav");
     last_scroll = window.scrollY;
@@ -14,4 +18,14 @@ window.addEventListener("scroll", function () {
     } else {
         navbar.classList.remove("nav__stick");
     }
+    //nav hiding when scrolling
+    let currentScrollPos = window.scrollY;
+
+    if (currentScrollPos < 200) {
+    } else if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navBar").style.top = "0";
+    } else {
+        document.getElementById("navBar").style.top = "-64px";
+    }
+    prevScrollpos = currentScrollPos;
 });
